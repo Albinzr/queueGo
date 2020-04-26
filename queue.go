88cebbd72dee4910887c38372d430a69381b1f4e
Albin CR <Albinzr@gmail.com>
2020-04-26 12:36:10 +0530

    changed loggong

--- queue.go
+++ queue.go
@@ -152,9 +152,9 @@ func (c *Config) schedule(callback func(message string, fileName string), interv
 	for range ticker.C {
 		if !c.isReading {
 			c.isReading = true
-			fmt.Println("Reading file status**:", c.isReading)
 			var files = c.getAllFileFromDir()
 			if files != nil && len(files) > 0 {
+				fmt.Println("Reading file status**:", c.isReading)
 				fmt.Println("files available to read:", files)
 				for _, file := range files {
 					fileData := c.readFile(file.Name())
@@ -163,10 +163,11 @@ func (c *Config) schedule(callback func(message string, fileName string), interv
 				}
 				fmt.Println("finished reading***................")
 				c.isReading = false
+				fmt.Println("Reading file status**:", c.isReading)
 			} else {
 				c.isReading = false
 			}
-			fmt.Println("Reading file status**:", c.isReading)
+
 		}
 	}
 
